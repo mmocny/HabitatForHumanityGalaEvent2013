f9f9cb1d51202d2ad4b7a3ebdafdeb47e641e590
Braden Shepherdson <braden.shepherdson@gmail.com>
2013-10-10 15:35:26 -0400

    Convert wall anchor-scrolling code to Angular. Working!

--- scripts/controllers/gallery.js
+++ scripts/controllers/gallery.js
@@ -1,5 +1,6 @@
 
 angular.module('habitat').controller('gallery', function($scope, $routeParams, content) {
   $scope.content = content.gallery[$routeParams.id];
+  $scope.imgIndex = 0;
 });
 
--- scripts/controllers/wall.js
+++ scripts/controllers/wall.js
@@ -5,8 +5,25 @@ angular.module('habitat').controller('wall', function($scope, $rootScope, $route
   $rootScope.anchors = [];
   $scope.activeAnchor = 0;
 
+  $scope.prepareWall = function() {
+    var wallContentElement = document.getElementsByClassName('wall-content');
+    angular.element(wallContentElement[0]).bind('scroll', function() {
+      var scrollPosition = wallContentElement[0].scrollTop;
+      for (var i = 0; i < $rootScope.anchors.length; i++) {
+        if (scrollPosition <= $rootScope.anchors[i].scrollPosition) {
+          $scope.activeAnchor = i;
+          break;
+        }
+      }
+      // Check for changes to the scope.
+      // TODO: Is this necessary when prepareWall is called by ngInclude's onload?
+      $scope.$apply();
+    });
+  };
+
   $scope.showAnchor = function(index) {
-    // TODO: Implement me.
-    console.log('showing anchor ' + index);
+    var a = $rootScope.anchors[index];
+    a.element.scrollIntoView();
+    $scope.activeAnchor = index;
   };
 });
