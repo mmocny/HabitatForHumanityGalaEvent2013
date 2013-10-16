a84c3829c52aff1f5212dfc041aa58b2fbfdc8fc
Michal Mocny <mmocny@gmail.com>
2013-10-16 13:57:43 -0400

    adding timeline to main app

--- scripts/timeline.js
+++ scripts/timeline.js
@@ -41,60 +41,53 @@ var timelineData = [
   year: '1988',
 }];
 
-var dotEls;
-var titleEls;
-var yearEls;
+var dotEls = [];
+var yearEls = [];
 var activeDotIndex = -1;
+var kTimelinePaddingPercent = 8;
 
 function timelineInit() {
-  dotEls = [].slice.call(document.getElementsByClassName('timeline-dot-container'));
-  titleEls = [].slice.call(document.getElementsByClassName('timeline-title'));
-  yearEls = [].slice.call(document.getElementsByClassName('timeline-year-container'));
+  var container = document.getElementsByClassName('timeline-line-container')[0];
+  var template = container.firstElementChild;
+  container.removeChild(template);
 
   for (var i = 0; i < timelineData.length; ++i) {
     var entry = timelineData[i];
-    var leftPercent = (10 + 80 * i / timelineData.length) + '%';
-    titleEls[i].textContent = entry.title;
+    var leftPercent = (kTimelinePaddingPercent + (100-kTimelinePaddingPercent) * i / timelineData.length) + '%';
+    var node = template.cloneNode(true);
+    dotEls.push(node);
     dotEls[i].style.left = leftPercent;
     dotEls[i].ontouchstart = onTimelineDotClick.bind(null, i);
     dotEls[i].onmousedown = onTimelineDotClick.bind(null, i);
-    yearEls[i].firstChild.textContent = entry.year;
     if (entry.familyStory) {
       dotEls[i].classList.add('timeline-dot-family');
     }
+    yearEls.push(node.firstElementChild);
+    yearEls[i].firstElementChild.textContent = entry.year;
+    container.appendChild(node);
   }
 
-  document.querySelector('.timeline-title-container').addEventListener('webkitTransitionEnd', onTitleTransitionEnd, false);
   onTimelineDotClick(0);
 }
 
-function onTitleTransitionEnd(e) {
-  if (e.target.classList.contains('timeline-title-inactive')) {
-    e.target.classList.remove('timeline-title-active');
-    e.target.classList.remove('timeline-title-inactive');
-    e.target.style.display='none';
-  }
-}
-
 function onTimelineDotClick(dotIndex) {
-  if (dotIndex == activeDotIndex) {
+  if (dotIndex === activeDotIndex)
     return;
-  }
+
   if (activeDotIndex >= 0) {
     dotEls[activeDotIndex].classList.remove('timeline-dot-active');
-    titleEls[activeDotIndex].classList.add('timeline-title-inactive');
     yearEls[activeDotIndex].style.display = 'none';
   }
 
-  setTimeout(function() {
-    titleEls[dotIndex].classList.add('timeline-title-active');
-    dotEls[dotIndex].classList.add('timeline-dot-active');
-  }, 0);
-  titleEls[dotIndex].style.display='';
-  yearEls[dotIndex].style.display = '';
-
   activeDotIndex = dotIndex;
+
+  setTimeout(function() {
+    dotEls[activeDotIndex].classList.add('timeline-dot-active');
+  }, 0);
+  yearEls[activeDotIndex].style.display = '';
+
+  document.getElementById('title').textContent = timelineData[activeDotIndex].title;
 }
 
-timelineInit();
+document.addEventListener('DOMContentLoaded', timelineInit);
 }()
