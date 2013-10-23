01d6d7be06a5d3971ee5c806e01a1692e26ef2a3
Michal Mocny <mmocny@gmail.com>
2013-10-23 11:25:46 -0400

    timeline changes to be larger icons

--- scripts/directives/timeline.js
+++ scripts/directives/timeline.js
@@ -7,60 +7,58 @@ angular.module('habitat').directive('hbTimeline', function($rootScope, $location
       year: 'Tonight',
       hash: '/wall/gala',
     }, {
-      title: '25 Years of HFHWR',
+      title: '2013: 25 Years of HFHWR',
       year: '2013',
       hash: '/wall/25Years',
     }, {
-      title: 'Terry Ann Thompson',
+      title: '2012: Terry Ann Thompson',
       year: '2012',
       familyStory: true,
       hash: '/gallery/family_thompson',
     }, {
-      title: '1st Condominium Project (Cherry St)',
+      title: '2006-2009: 1st Condominium Project (Cherry St)',
       year: '2006-2009',
       hash: '/gallery/first_condo',
     }, {
-      title: 'Cambridge ReStore Opens',
+      title: '2005: Cambridge ReStore Opens',
       year: '2005',
       hash: '/gallery/restore_cambridge',
     }, {
-      title: 'Pam and Roger Cruz',
+      title: '2003: Pam and Roger Cruz',
       year: '2003',
       familyStory: true,
       hash: '/wall/family_cruz',
     }, {
-      title: '1st Women\'s Build',
+      title: '2000: 1st Women\'s Build',
       year: '2000',
       hash: '/gallery/first_build_women',
     }, {
-      title: '1st Annual Charity Golf Tournament',
+      title: '1998: 1st Annual Charity Golf Tournament',
       year: '1998',
       hash: '/gallery/first_golf',
     }, {
-      title: '1st Cambridge Build',
+      title: '1996: 1st Cambridge Build',
       year: '1996',
       hash: '/gallery/first_build_cambridge',
     }, {
-      title: 'Jimmy Carter Work Project',
+      title: '1993: Jimmy Carter Work Project',
       year: '1993',
       hash: '/gallery/jimmy',
     }, {
-      title: 'Opening ReStore Waterloo',
+      title: '1993: Opening ReStore Waterloo',
       year: '1993',
       hash: '/gallery/restore_waterloo',
     }, {
-      title: 'Ellie and Ken Fields',
+      title: '1992: Ellie and Ken Fields',
       year: '1992',
       familyStory: true,
       hash: '/wall/family_fields',
     }, {
-      title: 'HFHWR is Incorporated',
+      title: '1988: HFHWR is Incorporated',
       year: '1988',
       hash: '/gallery/incorporated',
     }
   ];
-
-  var kTimelinePaddingPercent = 8;
 
   return {
     restrict: 'A',
@@ -76,8 +74,9 @@ angular.module('habitat').directive('hbTimeline', function($rootScope, $location
       '</div>',
     link: function(scope, elem, attrs) {
       scope.entries = timelineData;
+      var kTimelinePaddingPercent = (100/timelineData.length);
       for(var i = 0; i < timelineData.length; i++) {
-        scope.entries[i].style = (kTimelinePaddingPercent + (100-kTimelinePaddingPercent) * i / timelineData.length) + '%';
+        scope.entries[i].style = (kTimelinePaddingPercent + (100-(2*kTimelinePaddingPercent)) * i / timelineData.length) + '%';
       }
 
       scope.activeDotIndex = -1;
