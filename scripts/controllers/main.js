ff636a9a75fc3576167c124eeda472fec6db680f
Ian Clelland <iclelland@chromium.org>
2013-10-23 23:27:05 -0400

    Add slideshow mode: switch after 5 minutes unused

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -3,3 +3,29 @@ angular.module('habitat').controller('main', function($scope, content) {
   $scope.mainPage = content.mainPage;
 });
 
+
+var nextSlideTimeout = null;
+var startSlideshowTimeout = null;
+
+function stopSlideshow() {
+  clearTimeout(startSlideshowTimeout);
+  clearInterval(nextSlideTimeout);
+  startSlideshowTimeout = setTimeout(startSlideshow, 300000);
+}
+
+function startSlideshow() {
+  nextSlideTimeout = setInterval(nextSlide, 30000);
+}
+
+function nextSlide() {
+  var timelineElements = document.getElementsByClassName('timeline-dot');
+  var index = Math.floor(Math.random() * timelineElements.length);
+  var el = angular.element(timelineElements[index]);
+  el.triggerHandler('click');
+}
+
+stopSlideshow();
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.body.addEventListener('touchstart', stopSlideshow);
+});
